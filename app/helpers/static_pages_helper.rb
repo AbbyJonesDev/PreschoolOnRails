14623337e8f1632e98978bd0878fe835f3fdf3dd
Abigail Jones <AbbyJonesDev@gmail.com>
2014-10-06 20:51:26 -0500

    Add parent banner and footer

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,19 @@
 module StaticPagesHelper
+
+  def render_banner
+    if controller_name == 'static_pages'
+      render 'layouts/banner'
+    else
+      render 'layouts/parent_banner'
+    end
+  end
+
+  def render_footer
+    if controller_name == 'static_pages'
+      render 'layouts/footer'
+    else
+      render 'layouts/parent_footer'
+    end
+  end
+
 end
