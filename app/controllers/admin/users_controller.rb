72282683ec894d69accf69a4d883ac60f9e0c649
Abigail Jones <AbbyJonesDev@gmail.com>
2014-12-14 17:09:59 -0600

    Add parent from index in progress

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,20 @@
 class Admin::UsersController < Admin::DashboardController
   def index
-    @user = User.new
+    @user = User.new 
     @parents = User.where(role: "parent")
+    @students = Student.all
+    @groups = Group.all
   end
 
   def edit
     @user = User.find(params[:id])
   end
 end
+
+
+
+# Use Devise.friendly_token to auto generate a default password
+# Set default password in create action if none is specified in params
+# Make password field hidden - accessible by clicking link to view
+
+# Check Devise docs for how to add additional user field
--- spec/features/admin/account_manager_spec.rb
+++ spec/features/admin/account_manager_spec.rb
@@ -13,24 +13,44 @@ describe "Account Manager" do
       visit admin_parents_path
     end
 
-    it "has form to add parent" 
+    describe "Form to Add Parent" do
+      context "With Complete/Valid Inputs" do
+        before do 
+          fill_in("First Name", with: "Harriette")
+          fill_in("Last Name", with: "Winslow")
+          select("Parent", from: "Role")
+          choose("user_active_true") #Radio button - Active
+          fill_in("Email", with: "hwinslow@example.com")
+          # Password field - hidden
+          # Select student - optional
+          select("#{Group.last.name}", from: "Class")
+          click_on("Create")
+        end
 
-    it "displays table of users" do
-      expect(page).to have_selector("table tr", count: 3)
-    end
+        it "creates parent" do
+          expect(User.last.email).to eq("hwinslow@example.com")
+        end
+      end
+    end 
 
-    it "gives class name for parent" do
-      expect(page).to have_selector('td', text: "#{Group.first.name}")
-    end
+    describe "Table of Users" do
+      it "displays table of users" do
+        expect(page).to have_selector("table tr", count: 3)
+      end
 
-    it "has links to edit each user" do
-      id = User.find_by_role("parent").id #Will get first one
-      click_on("Edit", :match => :first)
-      expect(current_path).to eq(edit_admin_parent_path(id))
-    end
+      it "gives class name for parent" do
+        expect(page).to have_selector('td', text: "#{Group.first.name}")
+      end
 
-    it "has links to delete each user" do
-      expect(page).to have_link("Delete")
+      it "has links to edit each user" do
+        id = User.find_by_role("parent").id #Will get first one
+        click_on("Edit", :match => :first)
+        expect(current_path).to eq(edit_admin_parent_path(id))
+      end
+
+      it "has links to delete each user" do
+        expect(page).to have_link("Delete")
+      end
     end
   end
 end
