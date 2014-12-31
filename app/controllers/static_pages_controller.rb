34f5e456d378a4649982821f64b6771a40a1fbd8
Abigail Jones <AbbyJonesDev@gmail.com>
2014-12-31 16:12:28 -0600

    Remove unneeded variables

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,40 +1,27 @@
 class StaticPagesController < ApplicationController
 
   def home
-    # Set "current" class on link in view
-    @home = true
   end
 
   def about_us
-    # Set "current" class on link in view
-    @about = true
     # Set class for background color
     @body_background = "light-blue-background"
   end
 
   def activities
-    # Set "current" class on link in view
-    @activities = true
     # Set class for background color
     @body_background = "blue-background"
   end
 
   def photo_gallery
-    # Set "current" class on link in view
-    @photo = true
     # Set class for background color
     @body_background = "orange-background"
   end
 
   def register
-    # Set "current" class on link in view
-    @reg = true
-    # Set link addresses for pdf files
   end
 
   def schedule
-    # Set "current" class on link in view
-    @sched = true
     # Set class for background color
     @body_background = "orange-background"
   end
