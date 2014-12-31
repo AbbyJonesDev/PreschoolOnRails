74af424b03633634a54c1e9b804ec07e2254d057
Abigail Jones <AbbyJonesDev@gmail.com>
2014-12-30 19:33:37 -0600

    Switch from Gmail to Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ PreschoolOnRails::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
     domain:               'heroku.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
   config.action_mailer.default_url_options = { 
