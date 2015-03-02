60acc88ee1f3304d3302b033dfc8c0d9d71b34b1
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-01 19:35:15 -0600

    Groundwork for future refactoring

--- app/controllers/parents/class_contact_lists_controller.rb
+++ app/controllers/parents/class_contact_lists_controller.rb
@@ -2,6 +2,11 @@ class Parents::ClassContactListsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    # Most parents will only belong to one class and
+    # only have one class contact list accessible
+    # But some could belong to multiple classes
+    # Currently uses current_user in the view and
+    # Pulls everything else from that...
   end
 
   def show
