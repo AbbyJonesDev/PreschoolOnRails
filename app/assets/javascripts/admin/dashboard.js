6883660a4184f6264143628519123bd377851a25
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-05 17:19:16 -0600

    Non-optimal date display in edit view on Safari

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -1,9 +1,10 @@
 if (!Modernizr.inputtypes.date) {
+    var dateInput = $('input[type=date]');
     // Insert hidden field - needed to convert
     // jQuery Datepicker format to format Rails accepts
-    $('input[type=date]').after('<input id="hidden-date-field" type="hidden" value="" name="announcement[expires_on]">');
+    dateInput.after('<input id="hidden-date-field" type="hidden" value="" name="announcement[expires_on]">');
     // Initialize datepicker and formats
-    $('input[type=date]').datepicker({
+    dateInput.datepicker({
       dateFormat: 'mm-dd-yy',
       altFormat: "yy-mm-d",
       altField: "#hidden-date-field",
