92999662503a147279ff2d86a459be8807e91db3
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-05 16:21:38 -0600

    Date picker working in both browsers. Value not showing in Chrome

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -1,5 +1,11 @@
 if (!Modernizr.inputtypes.date) {
+    // Insert hidden field - needed to convert
+    // jQuery Datepicker format to format Rails accepts
+    $('input[type=date]').after('<input id="hidden-date-field" type="hidden" value="" name="announcement[expires_on]">');
+    // Initialize datepicker and formats
     $('input[type=date]').datepicker({
-      dateFormat: 'mm-dd-yy'
+      dateFormat: 'mm-dd-yy',
+      altFormat: "yy-mm-d",
+      altField: "#hidden-date-field",
     });
 }
