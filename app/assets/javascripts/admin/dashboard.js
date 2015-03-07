59411636874094383d8a334c2102fea6ed5c6d04
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-07 13:56:40 -0600

    Use jQuery datepicker with test field for all browsers

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -1,12 +1,10 @@
-if (!Modernizr.inputtypes.date) {
-    var dateInput = $('input[type=date]');
-    // Insert hidden field - needed to convert
-    // jQuery Datepicker format to format Rails accepts
-    dateInput.after('<input id="hidden-date-field" type="hidden" value="" name="announcement[expires_on]">');
-    // Initialize datepicker and formats
-    dateInput.datepicker({
-      dateFormat: 'mm-dd-yy',
-      altFormat: "yy-mm-d",
-      altField: "#hidden-date-field",
-    });
-}
+// Use jQuery Datepicker in all browsers
+// For consistent user experience
+// Alt field and format needed to populate
+// hidden field with Rails-friendly format
+var dateInput = $('#visible-date-field');
+dateInput.datepicker({
+  dateFormat: 'mm-dd-yy',
+  altFormat: "yy-mm-d",
+  altField: "#hidden-date-field",
+});
