f5f153f93788e1cb5d90339ab8d98bd34369923f
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-12 12:59:00 -0500

    Refine and refactor

--- app/controllers/parents/class_contact_lists_controller.rb
+++ app/controllers/parents/class_contact_lists_controller.rb
@@ -2,11 +2,14 @@ class Parents::ClassContactListsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    # Most parents will only belong to one class and
-    # only have one class contact list accessible
-    # But some could belong to multiple classes
-    # Currently uses current_user in the view and
-    # Pulls everything else from that...
+    @groups = Group.joins(:parents).
+      where('class_parents.user_id = ?', current_user).
+      includes(:class_contact_list)
+    # Most parents will only belong to one group
+    if @groups.length == 1
+      @list = @groups[0].class_contact_list
+      render :show
+    end
   end
 
   def show
