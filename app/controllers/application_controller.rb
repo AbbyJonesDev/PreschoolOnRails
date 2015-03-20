6d054bc146eb7bb7699928f4677dbdcb30529b59
Abigail Jones <AbbyJonesDev@gmail.com>
2015-03-19 20:13:50 -0500

    Message for 0 announcments instead of redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.role == "admin"
       admin_root_path
+    elsif Announcement.for_user(resource).length == 0
+      parents_peek_current_path
     else
       parents_announcements_path
     end
--- app/controllers/parents/announcements_controller.rb
+++ app/controllers/parents/announcements_controller.rb
@@ -3,9 +3,5 @@ class Parents::AnnouncementsController < ApplicationController
 
   def index
     @announcements = Announcement.for_user(current_user)
-    if @announcements.length == 0
-      flash[:notice] = "No announcements at this time"
-      redirect_to parents_root_path and return
-    end
   end
 end
