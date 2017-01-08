56e26d45ad7615bb9c15bcb4f9111241569444cc
Abby Jones <AbbyJonesDev@gmail.com>
2017-01-08 13:09:04 -0600

    Set herewegrowlincoln as domain for smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 '587',
-    domain:               'heroku.com',
+    domain:               'herewegrowlincoln.com',
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
